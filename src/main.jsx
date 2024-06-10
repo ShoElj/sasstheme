ca1ad513eb587ed6516e43526eaa376d3ea1931c
Elijah Shodipo <114405895+ShoElj@users.noreply.github.com>
2024-06-10 15:37:03 +0000

    Made  a few changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header.jsx";
-import Analytics from "./components/Analytics.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
-    <Analytics />
   </React.StrictMode>,
 );
