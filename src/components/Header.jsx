1c9462313d89c9f0bed736764daa229c3704fdda
Elijah Shodipo <114405895+ShoElj@users.noreply.github.com>
2024-06-10 18:47:55 +0000

    addded code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Analytics from "./src/components/Analytics";
+
 const navigation = [
   { name: "Product", href: "#" },
   { name: "Features", href: "#" },
