6c80c3147f43f7b8f319f14438686cc5ce44f278
shoElj <shodipoelijah2006@gmail.com>
2024-06-10 14:54:06 +0100

    second commit

---
+++ src/components/SaleBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SaleBanner = () => {
+  return (
+    <div className="bg-blue-500 text-white text-center py-2">
+      Use LAUNCH25 for a 25% discount! <button className="ml-2 bg-gray-800 py-1 px-3 rounded">Buy Now</button>
+    </div>
+  );
+};
+
+export default SaleBanner;
