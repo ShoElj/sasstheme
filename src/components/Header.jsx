1a1d0d8a7dc1db3807d6c6c41640de9ec8e9a836
Elijah Shodipo <114405895+ShoElj@users.noreply.github.com>
2024-06-10 19:11:49 +0000

    Added a new component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import AnalyticsSection from "./AnalyticsSection";
+import Analytics from "./Analytics";
+import WorkWithUs from "./components/WorkWithUs";
 
 const navigation = [
   { name: "Product", href: "#" },
@@ -121,39 +122,39 @@ export default function Example() {
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                "polygon(74.8% 44.1%, 100% 69.4%, 97.3% 38.5%, 92.3% 31.2%, 87.5% 25.5%, 76.3% 0%, 57.2% 0%, 45.6% 12.7%, 33.1% 22.8%, 20.8% 24.3%, 0% 45.4%, 8.8% 75.6%, 22.7% 68.6%, 36.5% 69.9%, 51.8% 85.3%, 62.6% 100%, 74.8% 44.1%)",
             }}
           />
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="mx-auto max-w-3xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            <div className="relative overflow-hidden rounded-full bg-gray-800 py-1.5 px-4 text-sm leading-6 text-gray-300 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Announcing our next round of funding.{" "}
-              <a href="#" className="font-semibold text-indigo-600">
+              <a href="#" className="font-semibold text-white">
                 <span className="absolute inset-0" aria-hidden="true" />
                 Read more <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
           </div>
           <div className="text-center">
-            <h1 className="text-6xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Revolutionize your business with our powerful SaaS tool
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Data to enrich your online business
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Foxtrot is a simple and powerful SaaS tool that helps you to track
-              your business analytics and improve your numbers. Just kidding,
-              it's just a template.
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+              fugiat aliqua.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
               </a>
               <a
                 href="#"
-                className="text-sm font-semibold leading-6 text-gray-900"
+                className="text-base font-semibold leading-7 text-gray-900"
               >
                 Learn more <span aria-hidden="true">→</span>
               </a>
@@ -168,12 +169,13 @@ export default function Example() {
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                "polygon(74.8% 44.1%, 100% 69.4%, 97.3% 38.5%, 92.3% 31.2%, 87.5% 25.5%, 76.3% 0%, 57.2% 0%, 45.6% 12.7%, 33.1% 22.8%, 20.8% 24.3%, 0% 45.4%, 8.8% 75.6%, 22.7% 68.6%, 36.5% 69.9%, 51.8% 85.3%, 62.6% 100%, 74.8% 44.1%)",
             }}
           />
         </div>
       </div>
-      <AnalyticsSection />
+      <Analytics />
+      <WorkWithUs />
     </div>
   );
 }
