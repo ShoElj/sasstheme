b68f8001b7ac46bf266e4ff19f5d03950e444728
shoElj <shodipoelijah2006@gmail.com>
2024-06-10 15:01:01 +0100

    added code to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Dialog, DialogPanel } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
-
+import SaleBanner from './components/SaleBanner.jsx'
 const navigation = [
   { name: 'Product', href: '#' },
   { name: 'Features', href: '#' },
